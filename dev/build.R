faf79cd399ae5d50be4172d7674ea4962143be7c
Ian Hussey <ianhusseysipod@gmail.com>
2024-07-17 13:38:39 +0200

    Create build.R

---
+++ dev/build.R
@@ -0,0 +1,24 @@
+
+library(roxygen2)
+# setwd("~/git")
+# devtools::create("ERROR")
+
+setwd("~/git/ERROR")
+
+devtools::document()
+
+devtools::build_vignettes()
+
+devtools::check()
+
+#devtools::install()
+
+# or from github, after push
+library(devtools)
+install_github("ianhussey/ERROR")
+
+library(ERROR)
+
+?ERROR
+
+detach("package:ERROR", unload=TRUE)
